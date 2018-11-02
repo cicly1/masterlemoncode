392084848093311d9729c24f51f93adbc86d6aa4
cicly1 <44709978+cicly1@users.noreply.github.com>
2018-11-02 21:35:08 +0000

    Update ArrayOperations.js

--- Ejercicios Lenguajes/ArrayOperations.js
+++ Ejercicios Lenguajes/ArrayOperations.js
@@ -1,20 +1,28 @@
 //1. Array operations
+//------------------------------------------
 //Head
+//------------------------------------------
 const head=['uno', 'dos', 'tres', 'cuatro', 'cinco', 'seis'];
 let resultado=head => head[0];
 console.log(resultado(head));
 
+//------------------------------------------
 //tail
+//------------------------------------------
 const tail=['uno', 'dos', 'tres', 'cuatro', 'cinco', 'seis'];
 var tailfun = arr=> arr.slice(1);
 console.log(tailfun(tail)); 
 
+//------------------------------------------
 //init
+//------------------------------------------
 const init=['uno', 'dos', 'tres', 'cuatro', 'cinco', 'seis'];
 var initfun = arr=> arr.slice(-arr.length,-1);
 console.log(initfun(init)); 
 
+//------------------------------------------
 //last
+//------------------------------------------
 const last=['uno', 'dos', 'tres', 'cuatro', 'cinco', 'seis'];
 var lastFun= arr =>   arr.splice(arr.length-1);
 console.log(lastFun(last));  
